34f5e50911cd72f4734ec33bda14be1cbd739010
elyse502 <elyseniyibizi502@gmail.com>
2025-02-07 06:58:12 +0200

    Add functionality to the course card features

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,18 @@ export const AppContextProvider = (props) => {
     setAllCourses(dummyCourses);
   };
 
+  // Function to calculate average rating of course
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
+    }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return totalRating / course.courseRatings.length;
+  };
+
   useEffect(() => {
     fetchAllCourses();
   }, []);
@@ -23,6 +35,7 @@ export const AppContextProvider = (props) => {
     currency,
     allCourses,
     navigate,
+    calculateRating,
   };
 
   return (
