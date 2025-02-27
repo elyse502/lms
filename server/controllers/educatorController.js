8bb9d7bc4cbe73911f059f29fff6430d05dfbaed
elyse502 <elyseniyibizi502@gmail.com>
2025-02-27 11:29:32 +0200

    Create a controller function to get educator dashboard data

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,6 +1,8 @@
 import { clerkClient } from "@clerk/express";
 import Course from "../models/Course.js";
 import { v2 as cloudinary } from "cloudinary";
+import { Purchase } from "../models/Purchase.js";
+import User from "../models/User.js";
 
 // Update role to educator
 export const updateRoleToEducator = async (req, res) => {
@@ -49,6 +51,57 @@ export const getEducatorCourses = async (req, res) => {
     const educator = req.auth.userId;
     const courses = await Course.find({ educator });
     res.json({ success: true, courses });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get Educator Dashboard Data (Total Earning, Enrolled Students, No. of Courses)
+export const educatorDashboard = async (req, res) => {
+  try {
+    const educator = req.auth.userId;
+    const courses = await Course.find({ educator });
+    const totalCourses = courses.length;
+
+    const courseIds = courses.map((course) => course._id);
+
+    // Calculate total earnings from purchases
+    const purchases = await Purchase.find({
+      courseId: { $in: courseIds },
+      status: "completed",
+    });
+
+    const totalEarnings = purchases.reduce(
+      (sum, purchase) => sum + purchase.amount,
+      0
+    );
+
+    // Collect unique enrolled student IDs with their course titles
+    const enrolledStudentsData = [];
+    for (const course of courses) {
+      const students = await User.find(
+        {
+          _id: { $in: course.enrolledStudents },
+        },
+        "name imageUrl"
+      );
+
+      students.forEach((student) => {
+        enrolledStudentsData.push({
+          courseTitle: course.courseTitle,
+          student,
+        });
+      });
+    }
+
+    res.json({
+      success: true,
+      dashboardData: {
+        totalEarnings,
+        enrolledStudentsData,
+        totalCourses,
+      },
+    });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
